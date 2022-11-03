8cda2becdde109a827f735f8ba68c7dd63c0b8e6
Max10969 <pig85888@gmail.com>
2022-11-03 15:34:04 +0800

    commit#3
    ．setup config for line and db
    ．create line test official account
    ．go line sdk (simple test done)

--- main.go
+++ main.go
@@ -2,38 +2,75 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/line/line-bot-sdk-go/v7/linebot"
+	"github.com/spf13/viper"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
 
-	// Set client options
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	viper.SetConfigName("config")
+	viper.SetConfigType("json")
+	viper.AddConfigPath("./config")
+	err := viper.ReadInConfig()
 
-	// Connect to MongoDB
+	if err != nil {
+		panic("Cannot read config file: " + err.Error())
+	}
+
+	// db connent start
+	dbConnectURI := "mongodb://" + viper.GetString("DB.Host") + ":" + viper.GetString("DB.Port")
+	clientOptions := options.Client().ApplyURI(dbConnectURI)
 	client, err := mongo.Connect(context.TODO(), clientOptions)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Check the connection
 	err = client.Ping(context.TODO(), nil)
 
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	fmt.Println("Connected to MongoDB!")
+	//db connect end
 
 	// gin setup test
 	r := gin.Default()
+
+	//line bot get message and reply the same one (simple test)
+	bot, err := linebot.New(
+		viper.GetString("Line.Secret"),
+		viper.GetString("Line.Token"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r.POST("/callback", func(c *gin.Context) {
+		events, err := bot.ParseRequest(c.Request)
+		if err != nil {
+			if err == linebot.ErrInvalidSignature {
+				log.Print(err)
+			}
+			return
+		}
+		for _, event := range events {
+			if event.Type == linebot.EventTypeMessage {
+				switch message := event.Message.(type) {
+				case *linebot.TextMessage:
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+						log.Print(err)
+					}
+				}
+			}
+		}
+	})
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
